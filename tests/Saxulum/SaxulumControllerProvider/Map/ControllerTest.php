1d9e72f3e84a3a88c454e3fd28f30f1c54c6428e
Dominik Zogg <dominik.zogg@gmail.com>
2013-06-29 22:55:40 +0200

    add the first tests

---
+++ tests/Saxulum/SaxulumControllerProvider/Map/ControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Saxulum\SaxulumControllerProvider\Map;
+
+class ControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testScenario1()
+    {
+        $namespace = 'Saxulum\SaxulumControllerProvider\Controller\TestController';
+        $serviceId = 'saxulum.saxulumcontrollerprovider.controller.testcontroller';
+
+        $controller = new Controller();
+        $controller->setNamespace($namespace);
+        $controller->setServiceId($serviceId);
+        $controller->setInjectContainer(true);
+
+        $this->assertEquals($namespace, $controller->getNamespace());
+        $this->assertEquals($serviceId, $controller->getServiceId());
+        $this->assertFalse($controller->hasInjectionKeys());
+        $this->assertTrue($controller->isInjectContainer());
+        $this->assertFalse($controller->hasMethods());
+        $this->assertEquals($controller, new Controller($controller->__toArray()));
+    }
+
+    public function testScenario2()
+    {
+        $namespace = 'Saxulum\SaxulumControllerProvider\Controller\TestController';
+        $serviceId = 'saxulum.saxulumcontrollerprovider.controller.testcontroller';
+        $injectionKeys = array('doctrine', 'twig');
+
+        $controller = new Controller();
+        $controller->setNamespace($namespace);
+        $controller->setServiceId($serviceId);
+        $controller->setInjectionKeys($injectionKeys);
+
+        $this->assertEquals($namespace, $controller->getNamespace());
+        $this->assertEquals($serviceId, $controller->getServiceId());
+        $this->assertEquals($injectionKeys, $controller->getInjectionKeys());
+        $this->assertTrue($controller->hasInjectionKeys());
+        $this->assertFalse($controller->isInjectContainer());
+        $this->assertEquals($controller, new Controller($controller->__toArray()));
+    }
+
+    public function testScenario3()
+    {
+        $namespace = 'Saxulum\SaxulumControllerProvider\Controller\TestController';
+        $serviceId = 'saxulum.saxulumcontrollerprovider.controller.testcontroller';
+
+        $methodArray = array(
+            'name' => 'setDoctrine',
+            'injectionKeys' => array('doctrine'),
+        );
+        $method = $this->getMock('Saxulum\SaxulumControllerProvider\Map\Method');
+
+        $method
+            ->expects($this->any())
+            ->method('__toArray')
+            ->will($this->returnValue($methodArray))
+        ;
+
+        $controller = new Controller();
+        $controller->setNamespace($namespace);
+        $controller->setServiceId($serviceId);
+        $controller->addMethod($method);
+
+        $this->assertEquals($namespace, $controller->getNamespace());
+        $this->assertEquals($serviceId, $controller->getServiceId());
+        $this->assertFalse($controller->hasInjectionKeys());
+        $this->assertFalse($controller->isInjectContainer());
+        $this->assertTrue($controller->hasMethods());
+
+        $newController = new Controller($controller->__toArray());
+
+        $this->assertEquals($controller->__toArray(), $newController->__toArray());
+    }
+}
---
+++ tests/Saxulum/SaxulumControllerProvider/Map/MethodTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Saxulum\SaxulumControllerProvider\Map;
+
+class MethodTest extends \PHPUnit_Framework_TestCase
+{
+    public function testScenario1()
+    {
+        $name = 'setDoctrine';
+        $injectionKeys = array('doctrine');
+
+        $method = new Method();
+        $method->setName($name);
+        $method->setInjectionKeys($injectionKeys);
+
+        $this->assertEquals($name, $method->getName());
+        $this->assertEquals($injectionKeys, $method->getInjectionKeys());
+        $this->assertTrue($method->hasInjectionKeys());
+        $this->assertEquals($method, new Method($method->__toArray()));
+    }
+}
